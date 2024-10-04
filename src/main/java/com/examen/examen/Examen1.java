d85ae807eb515f361c060f6288b679d6b0cdcd25
IVÁN FUSTER SIMARRO <ifussim@outlook.com>
2024-10-04 18:57:40 +0200

    1º ejercicio

--- src/main/java/com/examen/examen/Examen1.java
+++ src/main/java/com/examen/examen/Examen1.java
@@ -1,9 +1,36 @@
 package com.examen.examen;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+
 public class Examen1 {
 
     public void ejercicio1() {
-        
-    }
 
+        int num = 0;
+        boolean entradaValida = false;
+    
+        try (Scanner entrada = new Scanner(System.in)) {
+           while (!entradaValida) {
+             try {
+                System.out.println("Introduce un número");
+                num = entrada.nextInt();
+                entradaValida = true;
+   
+
+             } catch (InputMismatchException e) {
+                    System.out.println("ERROR: Introduce un Valor Valido...");
+                    entrada.next();
+             }
+
+            }
+            if (num < 0){
+                num = (num  * -1);
+
+             }
+
+             System.out.println("El Valor absoluto es " + num);
+        }
+    }
 }
